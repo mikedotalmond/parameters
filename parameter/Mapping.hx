d7a34d66083567d967be75e02c4e8652f8b8df72
mike <mikedotalmond@googlemail.com>
2015-11-20 15:11:26 +0000

    1st

---
+++ parameter/Mapping.hx
@@ -0,0 +1,248 @@
+﻿package parameter;
+
+/**
+ * ...
+ * @author Mike Almond
+ * @see parameter.Parameter for usage
+ */
+
+interface Interpolation { }
+interface InterpolationNone extends Interpolation { }
+interface InterpolationLinear extends Interpolation { }
+interface InterpolationExponential extends Interpolation { }
+
+interface IMapping<T,I> {
+
+	var min(default, null):T;
+	var max(default, null):T;
+	var range(get, never):T;
+
+	function setMinMax(min:T, max:T):Void;
+
+	/**
+	 * @param	normalisedValue (0-1)
+	 * @return	A mapped value in the range of min-max
+	 */
+	function map(normalisedValue:Float):T;
+
+	/**
+	 * @param	value	A value in the range of min-max
+	 * @return	Normalised value (0-1)
+	 */
+	function mapInverse(value:T):Float;
+
+	/**
+	 *
+	 * @return
+	 */
+	function toString():String;
+}
+
+
+@:multiType
+@:forward(min, max, setMinMax, range, map, mapInverse, toString)
+abstract Mapping<T,I>(IMapping<T,I>) {
+
+	public function new(minValue:T, maxValue:T);
+
+	@:to static inline function toMapBool<T,I>(t:IMapping<Bool, InterpolationNone>, minValue:Bool, maxValue:Bool):MapBool {
+		return new MapBool(minValue, maxValue);
+	}
+	@:to static inline function toMapIntLinear<T,I>(t:IMapping<Int, InterpolationLinear>, minValue:Int, maxValue:Int):MapIntLinear {
+        return new MapIntLinear(minValue, maxValue);
+    }
+	@:to static inline function toMapIntExponential<T,I>(t:IMapping<Int, InterpolationExponential>, minValue:Int, maxValue:Int):MapIntExponential {
+        return new MapIntExponential(minValue, maxValue);
+    }
+	@:to static inline function toMapFloatLinear<T,I>(t:IMapping<Float, InterpolationLinear>, minValue:Float, maxValue:Float):MapFloatLinear {
+        return new MapFloatLinear(minValue, maxValue);
+    }
+	@:to static inline function toMapFloatExponential<T,I>(t:IMapping<Float, InterpolationExponential>, minValue:Float, maxValue:Float):MapFloatExponential {
+        return new MapFloatExponential(minValue, maxValue);
+    }
+}
+
+
+class MapBool implements IMapping<Bool, InterpolationNone> {
+
+	public var min(default, null):Bool;
+	public var max(default, null):Bool;
+
+	public var range(get,never):Bool;
+	inline function get_range() return max != min;
+
+	public function new(min, max) {
+		setMinMax(min, max);
+	}
+
+	inline public function setMinMax(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	inline public function map(normalizedValue:Float) return normalizedValue == 1.0 ? max : min;
+	inline public function mapInverse(value:Bool) return value == max ? 1.0 : .0;
+
+	inline public function toString() return '[MapBool]';
+}
+
+
+class MapIntExponential implements IMapping<Int, InterpolationExponential> {
+
+	public var min(default, null):Int;
+	public var max(default, null):Int;
+
+	public var range(get , never):Int;
+	inline function get_range() return max - min;
+
+	var _min:Float;
+	var _max:Float;
+	var _t0	:Float;
+	var _t1	:Float;
+	var _t2	:Float;
+
+	public function new(min = -1, max = 1){
+		setMinMax(min, max);
+	}
+
+	public function setMinMax(min,max) {
+		this.min = min;
+		this.max = max;
+
+		_t2 = 0;
+		if (min <= 0) _t2 = 1 + min * -1;
+
+		_min = min + _t2;
+		_max = max + _t2;
+
+		_t0 = Math.log(_max / _min);
+		_t1 = 1.0 / _t0;
+	}
+
+	/**
+	 *
+	 * @param	normalisedValue (0-1)
+	 * @return	A mapped value in the range of min-max
+	 */
+	inline public function map(normalisedValue:Float) return Math.round(_min * Math.exp( (normalisedValue) * _t0 ) - _t2);
+
+	/**
+	 *
+	 * @param	value	A value in the range of min-max
+	 * @return	Normalised value (0-1)
+	 */
+	inline public function mapInverse(value:Int) return Math.log((value + _t2) / _min) * _t1;
+
+	inline public function toString() return '[MapIntExponential] min:${min}, max:${max}';
+}
+
+
+class MapIntLinear implements IMapping<Int, InterpolationLinear> {
+
+	public var min(default, null):Int;
+	public var max(default, null):Int;
+
+	public var range(get , never):Int;
+	inline function get_range() return max - min;
+
+	public function new(min:Int = 0, max:Int = 1 ){
+		setMinMax(min, max);
+	}
+
+	inline public function setMinMax(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	inline public function map(normalisedValue:Float) return Math.round(min + normalisedValue * range);
+
+	inline public function mapInverse(value:Int) return (value - min) / range;
+
+	inline public function toString() return '[MapIntLinear] min:${min}, max:${max}';
+}
+
+
+class MapFloatLinear implements IMapping<Float, InterpolationLinear> {
+
+	public var min:Float;
+	public var max:Float;
+
+	public var range(get , never):Float;
+	inline function get_range() return max - min;
+
+	public function new(min:Float = 0, max:Float = 1) {
+		setMinMax(min, max);
+	}
+
+	inline public function setMinMax(min:Float, max:Float) {
+		this.min = min;
+		this.max = max;
+	}
+
+	/**
+	 *
+	 * @param	normalisedValue (0-1)
+	 * @return	A mapped value in the range of min-max
+	 */
+	public inline function map(normalisedValue:Float) return min + normalisedValue * range;
+
+	/**
+	 *
+	 * @param	value	A value in the range of min-max
+	 * @return	Normalised value (0-1)
+	 */
+	public inline function mapInverse(value:Float) return ( value - min ) / range;
+
+	public inline function toString() return '[MapFloatLinear] min:' + min + ', max:' + max;
+}
+
+
+class MapFloatExponential implements IMapping<Float, InterpolationExponential> {
+
+	public var min(default, null):Float;
+	public var max(default, null):Float;
+
+	public var range(get, never):Float;
+	inline function get_range() return max - min;
+
+	var _min:Float;
+	var _max:Float;
+	var _t0	:Float;
+	var _t1	:Float;
+	var _t2	:Float;
+
+	public function new(min = .0, max = 1.0 ){
+		setMinMax(min, max);
+	}
+
+	public function setMinMax(min,max) {
+		this.min = min;
+		this.max = max;
+
+		_t2 = 0;
+		if (min <= 0) _t2 = 1 + min * -1;
+
+		_min = min + _t2;
+		_max = max + _t2;
+
+		_t0 = Math.log(_max / _min);
+		_t1 = 1.0 / _t0;
+	}
+
+	/**
+	 *
+	 * @param	normalisedValue (0-1)
+	 * @return	A mapped value in the range of min-max
+	 */
+	inline public function map(normalisedValue:Float) return _min * Math.exp( (normalisedValue) * _t0 ) - _t2;
+
+	/**
+	 *
+	 * @param	value	A value in the range of min-max
+	 * @return	Normalised value (0-1)
+	 */
+	inline public function mapInverse(value:Float) return Math.log((value + _t2) / _min) * _t1;
+
+	inline public function toString() return '[MapFloatExponential] min:${min}, max:${max}';
+
+}
---
+++ parameter/Parameter.hx
@@ -0,0 +1,213 @@
+﻿/**
+ * Parameter / Mapping
+ * @author Mike Almond
+ * 
+ * Using Haxe macro goodness to provide strongly typed Parameter/Observer stuff for Float/Int/Bool types.
+ * 
+ * Uses / depends on hxsignal
+ * 
+ * Parameter/Mapping idea based on part of Andre Michelle's Tonfall library
+ * https://code.google.com/p/tonfall/
+ * 
+ *	ParameterBase<T,I>
+ * 
+ *		T - Float,Int,Bool
+ *		I - InterpolationNone, InterpolationLinear, InterpolationExponential
+ *		
+ *		new Parameter<T,I>(name:String, min:T, max:T);
+ *		
+ *			.getValue(normalised:Bool = false):T;
+ *			.setValue(value:T, normalised:Bool = false, forced:Bool = false):Void;
+ *			
+ *			.defaultValue:T;
+ *			.setDefault(value:T, normalised:Bool = false):Void;
+ *			.setToDefault():Void;
+ *			
+ *			.addObserver(callback:Parameter<T,I>->Void, triggerImmediately = false, once = false);
+ *			.removeObserver(callback:Parameter<T,I>->Void);
+ *	
+ *	
+ *	Examples...
+ *	
+ *	var myFloat	= new Parameter<Float, InterpolationLinear>('aLinearFloatParameter', .0, 2*Math.PI);
+ *	var myFloat	= new Parameter<Float, InterpolationExponential>('anExponentialFloatParameter', -Math.PI, Math.PI);
+ *	
+ *	var myInt 	= new Parameter<Int, InterpolationLinear>('anLinearIntegerParameter', -64, 64);
+ *	var myInt 	= new Parameter<Int, InterpolationExponential>('anExponentialIntegerParameter', -64, 64);
+ *	
+ * 	var myBool 	= new Parameter<Int, InterpolationNone>('aBooleanParameter', false, true);
+ *	
+ *	myFloat.addObserver(function(p:Parameter) {
+ *		trace(p.getValue());
+ *		trace(p.getValue(true));
+ *	});
+ * 
+ *  myFloat.setValue(Math.PI);
+ *  myFloat.setValue(.5,true);
+ * 
+ */
+
+package parameter;
+
+import hxsignal.Signal;
+import parameter.Mapping;
+
+typedef BoolParameter = ParameterBase<Bool,Interpolation>
+typedef IntParameter = ParameterBase<Int,Interpolation>
+typedef FloatParameter = ParameterBase<Float,Interpolation>
+
+class ParameterBase<T,I> {
+
+	public var name(default, null):String;
+
+	public var defaultValue(default, null):T;
+
+	public var normalisedValue(default, null):Float;
+	public var normalisedDefaultValue(default, null):Float;
+	
+	public var mapping(default, null):Mapping<T,I>;
+	public var change(default, null):Signal<Parameter<T,I>->Void>;
+	
+	@:allow(parameter.Parameter)
+	private function new(name:String, mapping:Mapping<T,I>) {
+
+		this.name = name;
+		this.mapping = mapping;
+
+		change = new Signal<Parameter<T,I>->Void>();
+
+		setDefault(mapping.min);
+	}
+	
+	/**
+	 * 
+	 * @param	value
+	 * @param	normalised
+	 */
+	public function setDefault(value:T, normalised:Bool = false) {
+
+		var normValue;
+
+		if (normalised) {
+			normValue = cast value;
+			value = mapping.map(normValue);
+		} else {
+			normValue = mapping.mapInverse(value);
+		}
+
+		normalisedDefaultValue = normValue;
+		defaultValue = value;
+
+		setValue(cast normValue, true);
+	}
+
+	/**
+	 * 
+	 * @param	value
+	 * @param	normalised
+	 * @param	forced
+	 */
+	public function setValue(value:T, normalised:Bool = false, forced:Bool = false):Void {
+
+		var normValue;
+
+		if (normalised) normValue = cast value;
+		else normValue = mapping.mapInverse(value);
+
+		if (forced || normValue != normalisedValue) {
+			normalisedValue = normValue;
+			change.emit(cast this);
+		}
+	}
+
+	public function setToDefault() {
+		setValue(cast normalisedDefaultValue, true);
+	}
+
+	/**
+	 * 
+	 * @param	normalised
+	 * @return
+	 */
+	public function getValue(normalised:Bool = false):T {
+		if (normalised) return cast normalisedValue;
+		return mapping.map(normalisedValue);
+	}
+
+	/**
+	 * 
+	 * @param	observers
+	 * @param	triggerImmediately = false
+	 * @param	once = false
+	 */
+	public function addObservers(observers:Array<Parameter<T,I>->Void>, triggerImmediately = false, once = false) {
+		for (observer in observers) addObserver(observer, triggerImmediately, once);
+	}
+	
+	/**
+	 * 
+	 * @param	callback
+	 * @param	triggerImmediately = false
+	 * @param	once = false
+	 */
+	public function addObserver(callback:Parameter<T,I>->Void, triggerImmediately = false, once = false) {
+		if (!change.isConnected(callback)) {
+			change.connect(callback, once ? ConnectionTimes.Once : ConnectionTimes.Forever);
+		}
+		if (triggerImmediately) change.emit(cast this);
+	}
+	
+	/**
+	 * 
+	 * @param	callback
+	 */
+	public function removeObserver(callback:Parameter<T,I>->Void) {
+		if (change.isConnected(callback)) change.disconnect(callback);
+	}
+	
+	public function removeAllObservers() { change.disconnectAll(); }
+
+	public function toString():String {
+		return '[Parameter] ${name}, defaultValue:${defaultValue}, mapping:${mapping.toString()}';
+	}
+}
+
+
+@:multiType
+@:forward(name, defaultValue, normalisedValue, normalisedDefaultValue, mapping, change, setValue, getValue, setDefault, setToDefault, invert, addObservers, addObserver, removeObserver, removeAllObservers, toString)
+abstract Parameter<T,I>(ParameterBase<T,I>) {
+	
+	/**
+	 * Create a new Parameter using the type T (Float, Int, or Bool)
+	 * @param	name
+	 * @param	min
+	 * @param	max
+	 */
+    public function new(name:String, min:T, max:T);
+
+	@:to static inline function toBoolParameter<T,I>(t:ParameterBase<Bool,InterpolationNone>, name:String, min:Bool, max:Bool):BoolParameter {
+		return new BoolParameter(name, getBool(min, max));
+    }
+
+    @:to static inline function toIntParameter<T,I>(t:ParameterBase<Int,InterpolationLinear>, name:String, min:Int, max:Int):IntParameter {
+		return new IntParameter(name, getInt(min, max));
+    }
+	@:to static inline function toIntParameterExpo<T,I>(t:ParameterBase<Int,InterpolationExponential>, name:String, min:Int, max:Int):IntParameter {
+		return new IntParameter(name, getIntExponential(min, max));
+    }
+
+	@:to static inline function toFloatParameter<T,I>(t:ParameterBase<Float,InterpolationLinear>, name:String, min:Float, max:Float):FloatParameter {
+		return new FloatParameter(name, getFloat(min,max));
+    }
+	@:to static inline function toFloatParameterExpo<T,I>(t:ParameterBase<Float,InterpolationExponential>, name:String, min:Float, max:Float):FloatParameter {
+		return new FloatParameter(name, getFloatExponential(min,max));
+    }
+	
+	static function getBool(min,max) return cast new Mapping<Bool,InterpolationNone>(min, max);
+
+	static function getFloat(min,max) return cast new Mapping<Float,InterpolationLinear>(min, max);
+	static function getFloatExponential(min,max) return cast new Mapping<Float,InterpolationExponential>(min, max);
+
+	static function getInt(min,max) return cast new Mapping<Int,InterpolationLinear>(min, max);
+	static function getIntExponential(min,max) return cast new Mapping<Int,InterpolationExponential>(min, max);
+}
